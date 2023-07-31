ca668618c4a37e738254e70604263a758115082d
Invegorant <kraisyvirus@yandex.ru>
2023-07-31 08:06:38 +0700

    first commit

---
+++ Task2/FromZeroToFive.cs
@@ -0,0 +1,49 @@
+﻿namespace NoobCodersPractice.Task2;
+
+public class FromZeroToFive
+{
+    /*
+     * Напишите программу, в которой пользователь вводит с клавиатуры число от 0 до 5.
+     * Программа должна вывести счёт от введённого числа и до 5.
+     */
+
+    public static void Main()
+    {
+        Console.WriteLine("Please, enter the number from 0 to 5:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        //Тут будет Ваш код
+        switch (a)
+        {
+            case 0:
+                WriteNumbers(0);
+                break;
+            case 1:
+                WriteNumbers(1);
+                break;
+            case 2:
+                WriteNumbers(2);
+                break;
+            case 3:
+                WriteNumbers(3);
+                break;
+            case 4:
+                WriteNumbers(4);
+                break;
+            case 5:
+                WriteNumbers(5);
+                break;
+            default:
+                Console.WriteLine("You can't pass this number! It takes from 0 to 5 only!");
+                break;
+        }
+    }
+
+    public static void WriteNumbers(int number)
+    {
+        for (int i = number; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Task2/VowelsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace NoobCodersPractice.Task2;
+
+public class VowelsTest
+{
+    /*
+     * Напишите программу, в которой пользователь вводит с клавиатуры букву.
+     * Программа должна вывести "гласная" если введённая буква гласная.
+     * Если буква согласная, программа ничего не выводит. Пользователь вводит только русские строчные буквы.
+     */
+    
+   public static void Main()
+    {
+        Console.WriteLine("Введите букву:");
+        char letter = Convert.ToChar(Console.ReadLine());
+        switch (letter)
+        {
+            case 'а':
+                Console.WriteLine("гласная");
+                break;
+            case 'у':
+                Console.WriteLine("гласная");
+                break;
+            case 'о':
+                Console.WriteLine("гласная");
+                break;
+            case 'ы':
+                Console.WriteLine("гласная");
+                break;
+            case 'и':
+                Console.WriteLine("гласная");
+                break;
+            case 'э':
+                Console.WriteLine("гласная");
+                break;
+            case 'я':
+                Console.WriteLine("гласная");
+                break;
+            case 'ю':
+                Console.WriteLine("гласная");
+                break;
+            case 'ё':
+                Console.WriteLine("гласная");
+                break;
+            
+            case 'е':
+                Console.WriteLine("гласная");
+                break;
+        }
+    }
+}
